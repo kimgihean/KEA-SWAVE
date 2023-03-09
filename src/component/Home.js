564988d7ce9ec60d2d43428dad6327882a584359
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2023-03-09 09:00:19 +0900

    Add files via upload

---
+++ src/component/Home.js
@@ -0,0 +1,10 @@
+import logo_white from '../image/logo_white.png';
+
+export default function App() {
+  return (
+    <div>
+      <img src={logo_white} alt="BigCo Inc. logo"/>
+      <h2 id="HeaderFont">We Should Create Wave in Software</h2>
+    </div>
+  );
+}
